983c88954d30075842bc5551c2fae45f664cd42f
r.b4rr10s <a20202073@pucp.edu.pe>
2022-10-25 11:13:39 -0500

    Se agrego telesystem_aa.sql donde se encuentra listada la info para las tablas

---
+++ telesystem_aa.sql
@@ -0,0 +1,28 @@
+insert into TipoIncidencia(tipo) 
+		values ("Accidente"),
+			("Reporte de robos"),
+            ("Infraestuctura en mal estado"),
+            ("Objetos perdidos"),
+            ("Ambulancia PUCP"),
+            ("Otros");
+update TipoIncidencia set tipo="Infraestructura en mal estado" where idTipoIncidencia=3;
+
+insert into NivelUrgencia (nivel) values ("Leve"), ("Moderado"), ("Critico");
+insert into EstadoIncidencia (estado) values ("En proceso"),
+																	("Registrado"),
+																	("Atendido"),
+                                                                    ("Resuelto");
+insert into Roles (nombreRol) values ("Usuario PUCP"),("Seguridad");
+
+insert into CategoriaPUCP(nombreCategoria) values ("Alumno"),("Administrativo"),("Jefe de practica"),("Profesor"),("Egresado");
+
+insert into Usuarios (codigo, nombre, apellido, DNI, validaUsuario, password, nickname, idRoles, idCategoriaPUCP)
+					values (20201696, "Leonardo", "Abanto", 12345678, true, "clashroyale123","Ubuntu",1,1),
+                    (19905060,"Jorge","Gonzales",87654321, true,"tele1000", "master",1,4);
+                    
+insert into Incidencia (fecha,nombreIncidencia,zonaPUCP, latitud, longitud, validaIncidencia, descripcion,
+						 contadorReabierto, idTipoIncidencia, idNivelUrgencia, idEstadoIncidencia,
+                         codigoUsuario)
+                         values (now(),"Explosion en KILO", "Pabellon A",-12.073157,-77.081164,true,"Fuga de gas ocurrido en la mañana, previo al abrir al local",
+                         0,1,3,1,20201696),
+                         (now(),"Accidente de semana de Ingenieria","Cancha de minas",-12.072249,-77.081994,true,"Lesion de alumno en evento",0,1,2,1,19905060);                    
